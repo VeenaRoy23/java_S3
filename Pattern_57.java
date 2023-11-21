f6e413c5dfda750ddeaa104930cb3283a714bdc0
VeenaRoy23 <127951652+VeenaRoy23@users.noreply.github.com>
2023-11-21 21:26:32 +0530

    Update Pattern_57.java

--- Pattern_57.java
+++ Pattern_57.java
@@ -1,9 +1,13 @@
+import java.util.canner;
 class Pattern_57{
 	public static void main (String args[]){
-		int a=5;
-		for(int i=0;i<=a;i++)
+		Scanner s=new Scanner(System.in);
+		int a;
+		System.out.println("Enter the length");
+		a=s.nextInt();
+		for(int i=1;i<=a;i++)
 		{
-			for(int j=0;j<i;j++)
+			for(int j=1;j<i;j++)
 			{
 				System.out.print("*"+" ");
 			}
